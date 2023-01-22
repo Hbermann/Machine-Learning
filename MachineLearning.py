38aba3c7ac43ded8b6bff1ffb507879143ce6ffb
Heather Bermann <121829694+Hbermann@users.noreply.github.com>
2023-01-22 21:33:06 +0000

    Update MachineLearning.py

--- MachineLearning.py
+++ MachineLearning.py
@@ -61,6 +61,86 @@ print("X_test.shape = ", X_test.shape)
 print("y_train.shape = ", y_train.shape)
 print("y_test.shape = ", y_test.shape)
 
+
+def calculateDistance(observation, missing, output_indicies):
+    observation = observation.to_numpy()
+    missing = missing.to_numpy()
+    # print("size:" + str(observation.size))
+    return absolute_distance(observation, missing, output_indicies)
+    
+def absolute_distance(observation, missing, output_indicies):
+    sum = 0
+    for x in range(0,observation.size-1):
+        if(not(np.isnan(missing[x])) and not(x in output_indicies)): # do not calculate with outputs or null entries
+            sum +=np.abs(observation[x] - missing[x])
+    return sum
+    
+# df needs to not have missing values in itself
+def impute_missing_value (df, missing, n_neighbours, output_indicies):
+    distance = np.full(n_neighbours, np.inf)
+    index = np.empty(n_neighbours)
+    for x in range(0, len(df.index)):
+        # only add to distance array if there is a smaller value
+        
+        dist = calculateDistance(df.iloc[x], missing, output_indicies)
+        for y in range(0, n_neighbours):
+            if(dist < distance[y]):
+                distance[y] = dist
+                index[y] = x
+                break
+
+    # for missing values in the missing observation, fill in using majority category
+    # since no continuous data is missing
+
+    booleans = np.isnan(missing)
+    neighbours = df.iloc[index,:]
+    for x in range(0,len(booleans)):
+        if(booleans[x]):
+            # calculate the majority class for that feature in nearest neighbours
+            # find all the unique values for that feature
+            # fill in at index x
+            majority_x = neighbours.iloc[:,x].value_counts().idxmax()
+            missing[x] = majority_x
+    
+    print(missing)
+    return missing
+    
+
+def KNNimputation(df, n_neighbours, output_indicies):
+    # Find all rows with nan values
+    indices = df.index[df.isnull().any(axis=1)]
+    nan_df = df[df.isnull().any(axis=1)]
+    
+    for x in range(0,len(nan_df)):    
+        df.iloc[indices[x]] = impute_missing_value(df.drop([df.index[x]]), nan_df.iloc[x],n_neighbours, output_indicies)
+    
+    return df
+
+k_neighbours = 15
+y_train.reset_index(drop=True, inplace=True)
+y_test.reset_index(drop=True, inplace=True)
+output_cols = ["pCR (outcome)", "RelapseFreeSurvival (outcome)"]
+
+# Training imputation
+merged_train = pd.concat([X_train_normalised, y_train], axis = 1)
+output_indicies = [merged_train.columns.get_loc(col) for col in output_cols]
+knn_imputated_train_df = KNNimputation(merged_train, k_neighbours, output_indicies)
+
+
+# Testing imputation
+merged_test = pd.concat([X_test_normalised, y_test], axis = 1)
+output_indicies = [merged_test.columns.get_loc(col) for col in output_cols]
+knn_imputated_test_df = KNNimputation(merged_test, k_neighbours, output_indicies)
+
+
+# Overwriting the existing train/test partioned dataframes with the new imputed dataframes
+X_train = knn_imputated_train_df.drop(output_cols,axis=1)
+y_train = knn_imputated_train_df.drop(merged_test.drop(output_cols, axis = 1), axis = 1)
+X_test = knn_imputated_test_df.drop(output_cols, axis = 1)
+y_test = knn_imputated_test_df.drop(merged_test.drop(output_cols, axis = 1), axis = 1)
+
+knn_imputated_test_df.head()
+
 #%% Preprossesing and normalisation
 
 # define standard scaler model
@@ -91,3 +171,9 @@ X_train_normalised = pd.concat([X_train_Cat, normalised_X_train], axis=1)
 X_test_Cat.reset_index(drop=True, inplace=True)
 normalised_X_test.reset_index(drop=True, inplace=True)
 X_test_normalised = pd.concat([X_test_Cat, normalised_X_test], axis=1)
+
+# print number of values missing in each partition of the train/test data
+print("X_train_normalised missing data = ", X_train_normalised.isnull().sum().sum())
+print("X_test_normalised missing data = ", X_test_normalised.isnull().sum().sum())
+print("y_train missing data = ", y_train.isnull().sum().sum())
+print("y_test missing data = ", y_test.isnull().sum().sum())
